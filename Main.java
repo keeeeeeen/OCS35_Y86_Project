61bd5de5f6a3e132ed808da19c05afef9c1b23a4
1006mjkim <1006mjkim@gmail.com>
2025-02-19 15:37:28 +0900

    resolved symbolic names (to some extent)

--- Main.java
+++ Main.java
@@ -45,7 +45,19 @@ class Main{
         icode_ifun.put("mrmovq", "50");
         icode_ifun.put("addq", "60");
         icode_ifun.put("subq", "61");
-        //finish hash map later
+        icode_ifun.put("andq", "62");
+        icode_ifun.put("xorq", "63");
+        icode_ifun.put("jmp", "70");
+        icode_ifun.put("jle", "71");
+        icode_ifun.put("jl", "72");
+        icode_ifun.put("je", "73");
+        icode_ifun.put("jne", "74");
+        icode_ifun.put("jge", "75");
+        icode_ifun.put("jg", "76");
+        icode_ifun.put("call", "80");
+        icode_ifun.put("ret", "90");
+        icode_ifun.put("pushq", "A0");
+        icode_ifun.put("popq", "B0");
 
         HashMap<String, String> registers = new HashMap<String, String>();
         registers.put("%rax", "0");
@@ -54,9 +66,24 @@ class Main{
         registers.put("%rbx", "3");
         registers.put("%rsp", "4");
         registers.put("%rbp", "5");
+        //finish hash map later
 
+        HashMap<String, String> symbolic_names = new HashMap<String, String>();
+        int length, line_num = 0;
 
         for (i=0; code[i] != null; i++){
+            System.out.println("");
+            length = code[i].length();
+            //setting up symbolic names to a hashmap
+            //delete the colon and add an element to the hashmap e.g. main --> 0x0
+            //i'm assuming that the symbolic name declarations are done before they are used i.e. loop: is done before call loop - fix!
+            if(code[i].charAt(length-1) == ':'){
+                symbolic_names.put(code[i].replace(":", ""), ""+line_num);
+                continue;
+            }
+
+            line_num += 10;
+
             //split each line by a comma or a blank space
             String[] temp = code[i].split(", | ");
             temp[0] = icode_ifun.get(temp[0]);
@@ -64,7 +91,7 @@ class Main{
 
             char icode = temp[0].charAt(0);
             //set rA and rB for cmov and op
-            if (icode == '2' || icode == '4' || icode == '5' || icode == '6'){
+            if (icode == '2' || icode == '6'){
                 temp[1] = registers.get(temp[1]);
                 temp[2] = registers.get(temp[2]);
                 System.out.println(temp[1]);
@@ -72,22 +99,46 @@ class Main{
             }
 
             //set rA and rB for push and pop
-            if (icode == 'A' || icode == 'B'){
+            else if (icode == 'A' || icode == 'B'){
                 temp[1] = registers.get(temp[1]) + 'F';
             }
 
             //set rA and rB for irmovq
-            if (icode == '3'){
+            else if (icode == '3'){
                 temp[1] = 'F' + registers.get(temp[1]);
             }
 
-            //set rA, rB, and D for rmmovq and mrmovq
-            if (icode == '4' || icode == '5'){
-
+            //set rA, rB, and D for rmmovq
+            else if (icode == '4'){
+                temp[1] = registers.get(temp[1]);
+                //take away the displacement and parentheses e.g. 8(%rax) --> %rax
+                String[] register_displacement = temp[2].split("\\(");
+                temp[2] = register_displacement[1].replace(")", "");
+                temp[2] = registers.get(temp[2]);
+                //add displacement to the end of the string
+                temp[2] += register_displacement[0];
+                System.out.println(temp[1]);
+                System.out.println(temp[2]);
             }
 
-            //Setting up Symbolic names into hashmap
+            //set rA, rB, and D for mrmovq
+            else if (icode == '5'){
+                //take away the displacement and parentheses e.g. 8(%rax) --> %rax
+                String[] register_displacement = temp[1].split("\\(");
+                temp[1] = register_displacement[1].replace(")", "");
+                temp[1] = registers.get(temp[1]);
+
+                temp[2] = registers.get(temp[2]);
+                //add displacement to the end of the string
+                temp[2] += register_displacement[0];
+                System.out.println(temp[1]);
+                System.out.println(temp[2]);
+            }
             
+            else if (icode == '7' || icode == '8'){
+                temp[1] = symbolic_names.get(temp[1]);
+                System.out.println(temp[1]);
+            }
         }
     }
 }
